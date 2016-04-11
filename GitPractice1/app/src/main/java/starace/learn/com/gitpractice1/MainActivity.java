e18434ac4259ac0751cd69d44707a6bbfcd00598
Michael Starace <mstarace@Michaels-MacBook-Air-4.local>
2016-04-11 10:55:19 -0700

    GitPractice empty project

---
+++ GitPractice1/app/src/main/java/starace/learn/com/gitpractice1/MainActivity.java
@@ -0,0 +1,13 @@
+package starace.learn.com.gitpractice1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
